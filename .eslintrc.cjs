fe6565df02e0d3180409acb0843b7f43040e0ed4
Luke Bayliss <hello@lukebayliss.com>
2024-03-12 23:30:37 +1100

    Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: { node: true },
+  ignorePatterns: ['.*.js', 'node_modules/', 'dist/', '.husky'],
+  rules: {
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
+  },
+};
